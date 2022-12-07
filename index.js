3d9746ad32ae89f03daf950c3c1324e470e0d2c5
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-07 18:25:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <=400 ){
+    return 'This one is on me!';
+  }
+  else if(distance <2500){
+  return 'I will gladly take your thirty bucks.';
+  }  else if (distance > 400 && distance < 200){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(distance >2500){
+    return 'No can do.' ;
+
 }
 
-function ternaryCheckCity(){
+}
+function ternaryCheckCity(city){
   // Write your code here!
+return city =='NYC'?'Ok, sounds good.':'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+  default:
+  return 'Bye.';
+    break;
+}
 }
